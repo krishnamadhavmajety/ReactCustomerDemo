31412714b60f3fa25e7dd124d7b2428165c0a60f
krishnamadhavmajety <80944183+krishnamadhavmajety@users.noreply.github.com>
2021-03-28 22:03:27 +0530

    Edit, JSON Server changes

--- src/App.js
+++ src/App.js
@@ -1,68 +1,118 @@
-import Header from './Header'
 import './App.css';
-import {useState} from 'react'
-import Customers from './Customers';
-import AddCustomer from './AddCustomer';
+import Header from './components/Header';
+import AddCustomer from './components/AddCustomer';
+import { useEffect, useState } from 'react';
+import Customers from './components/Customers';
+import Footer from './components/Footer';
 
 function App() {
 
+  const JSON_SERVER_URL = "http://localhost:3005/customers/";
+  const [customers, setCustomers] = useState([]);
 
-  const deleteCustomer = (id) => {
-    console.log('Delete' ,id)
-    setCustomers(customers.filter((customer) => customer.id !== id))
-  } 
+  const [inEdit,setEdit] = useState(false)
+  const [cusById,setCusById] = useState(null);
 
-  const editCustomer = (id) => {
-    console.log('Edit' ,id)
-    //setCustomers(customers.filter((customer) => customer.id !== id))    
-      {<addCustomer />}
-  } 
+  const fetchCustomers = async()=>{
+    const res = await fetch(JSON_SERVER_URL)
+    const data = await res.json()
+    return data
+  }
 
-   const addCustomer =(customer) => {
-     const id = Math.random()
-     // spread operator
-     const newCustomer = {id, ...customer}
-     setCustomers([...customers,newCustomer])
+  const fetchCustomerById = async(id)=>{
+    const res = await fetch(JSON_SERVER_URL+id)
+    const data = res.json()
+    return data;
+  }
 
-   }
-   const [showAddCustomer,setShowAddCustomer] = useState(false)
+  useEffect(()=>{
+    const getCustomers = async()=>{
+      const data = await fetchCustomers()
+      setCustomers(data)
+    }
+    getCustomers()
+  })
 
+  const addCustomerOnClick = async(customer,isEdit)=>{
+    if(!isEdit){
+      const res = await fetch(JSON_SERVER_URL,{
+        method:'POST',
+        headers:{
+          "Content-type":'application/json'
+        },
+        body:JSON.stringify(customer)
+      })
+      const data = await res.json()
+      setCustomers([...customers,data])
+    }else{
+      
+      const editedCusObj = {
+        id:customer.id,
+        name:customer.name,
+        location:customer.location
+      }
+      const res = await fetch(JSON_SERVER_URL+customer.id,{
+        method:'PUT',
+        headers:{
+          'Content-type':'application/json'
+        },
+        body:JSON.stringify(editedCusObj)
+      })
 
+      const data = await res.json()
+      setCustomers([...customers,data])
+      setEdit(false)
+    }
+  }
 
-  const [customers,setCustomers] = useState(
-    [
-        {
-            "id": 1,
-            "name": "Cust Name1",
-            "location": "USA"            
-          },
-          {
-            "id": 2,
-            "name": "Cust Name2",
-            "location": "Hyd"            
-          },
-          {
-            "id": 3,
-            "name": "Cust Name3",
-            "location": "Delhi"                        
-          }
-    ]
+  const deleteCustomerOnClick = async(id)=>{
+    const res = await fetch(JSON_SERVER_URL+id,{
+      method:'DELETE'
+    })
+    const data = await res.json()
+    setCustomers([...customers,data])
+    
+  }
 
-)
+  const getCustomerById = (id)=>{
+    let returnCusObj;
+    customers.forEach((item)=>{
+      if(item.id === id){
+        returnCusObj = item;
+      }
+    })
+    return returnCusObj;
+  }
 
+  const editButtonClicked = (id)=>{
+    setEdit(true)
+    
+    console.log(inEdit)
+    setCusById(getCustomerById(id));
+    console.log(cusById)
+  }
 
   return (
-    <div className="container">
-      <Header title='Customer Info' 
-      onAdd={()=> setShowAddCustomer(!showAddCustomer)} 
-      showAdd={showAddCustomer}/>
-
-      {showAddCustomer && <AddCustomer onAdd={addCustomer} />}
-
-{customers.length > 0 ? <Customers customers={customers}
- onDelete={deleteCustomer} onEdit={editCustomer}
- /> :'No Customer to show'}
-
+    <div>
+      <div className='container'>
+        <div className='outer-box'>
+          <Header title = "Customer Info Demo"></Header>
+          <AddCustomer 
+            addCustomer = {addCustomerOnClick} 
+            inEditState = {inEdit}
+            cusById = {cusById}
+          />
+        </div>
+        { customers.length > 0 
+          ? <Customers 
+            customers={customers} 
+            deleteCus = {deleteCustomerOnClick} 
+            editButtonClicked = {editButtonClicked} 
+          /> 
+          : 'No Customers to display!!'
+        }
+      </div>
+      <Footer />
     </div>
   );
 }
